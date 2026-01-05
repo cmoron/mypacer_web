0535d0eabc3406a060c572789465ebd6c1493f02
Cyril Moron <cyril.moron@gmail.com>
2026-01-05 08:48:35 +0100

    fix: Ajouter .svelte-kit au .gitignore et nettoyer l'historique
    Le répertoire .svelte-kit/ contient des fichiers générés par le build
    et ne doit jamais être versionné. Ce commit :
    - Ajoute .svelte-kit à .gitignore
    - Supprime tous les fichiers .svelte-kit/ déjà commitées
    - Nettoie les assets dupliqués dans src/assets/

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CmhlSdBI.js","_app/immutable/chunks/DCZbF_Ch.js","_app/immutable/chunks/dyCKyNtP.js","_app/immutable/chunks/BiJUjwxP.js","_app/immutable/chunks/Cvv1SIiS.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.fsi2HpnT.js","_app/immutable/chunks/DCZbF_Ch.js","_app/immutable/chunks/dyCKyNtP.js","_app/immutable/chunks/Cvv1SIiS.js"];
-export const stylesheets = ["_app/immutable/assets/2.DlGkPquq.css"];
-export const fonts = [];
